e716a19f67cc75d4652088a8af0a2f3d93cdd945
orenovadia <oren@twiggle.com>
2019-09-24 11:40:26 -0700

    First working version of flattening with a path context

--- jflat.py
+++ jflat.py
@@ -1,3 +1,58 @@
+from __future__ import unicode_literals
+
+from contextlib import contextmanager
+
+
 def flatten_json_object(obj):
     # type: (dict) -> dict
-    return obj
+    return Flattener().flatten(obj)
+
+
+class Flattener(object):
+    def __init__(self) -> None:
+        self._tracker = PathTracker()
+        self._result = {}
+
+    def flatten(self, obj):
+        if not isinstance(obj, dict):
+            raise TypeError('Root must be a obj, got {}'.format(type(obj)))
+        self._visit(obj)
+        return self._result
+
+    def _visit(self, obj):
+        if isinstance(obj, dict):
+            self._visit_dict(obj)
+        else:
+            self._visit_value(obj)
+
+    def _visit_dict(self, obj):
+        for key, value in obj.items():
+            with self._tracker.inside(key):
+                self._visit(value)
+
+    def _visit_value(self, value):
+        self._result[self._tracker.path()] = value
+
+
+class CanNotBeFlattenedError(ValueError):
+    pass
+
+
+class PathTracker(object):
+    def __init__(self) -> None:
+        self._stack = []
+
+    @contextmanager
+    def inside(self, key):
+        self._stack.append(key)
+        try:
+            yield
+        finally:
+            self._stack.pop()
+
+    def path(self):
+        # This method is not optimal for balanced trees
+        # because we concat the same prefix multiple times
+        # However, it is more elegant than keeping all the prefixes at all times
+        # And might even be faster for workloads with unbalanced nesting
+        return '.'.join(self._stack)
--- test/test_flattener.py
+++ test/test_flattener.py
@@ -1,12 +1,24 @@
+from __future__ import unicode_literals
+
 from unittest import TestCase
 
-from jflat import flatten_json_object
+from jflat import flatten_json_object, CanNotBeFlattenedError
 
 
 class FlattenerTests(TestCase):
     def test_degenerate_case(self):
         self._assert_flatten({}, {})
         self._assert_flatten({'a': 1}, {'a': 1})
+
+    def test_root_must_be_an_object(self):
+        # for instance `null` is a legitimate json object
+        with self.assertRaises(TypeError):
+            flatten_json_object(None)
+
+    def test_all_types_of_leaf_values_are_legitimate(self):
+        values = [1, 1.2, None, float('inf'), 'str']
+        for value in values:
+            self._assert_flatten({'a': {'b': value}}, {'a.b': value})
 
     def _assert_flatten(self, nested, expected):
         actual = flatten_json_object(nested)
